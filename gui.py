25d61bd55af926e4a4391d70a34615a949d2325b
M E <matthias.engh@gmail.com>
2019-01-01 19:16:29 +0100

    begin drawing

--- gui.py
+++ gui.py
@@ -1,20 +1,53 @@
 import pygame
 import sys
 
-def main_loop(program_state, business):
-  while True:
-    events = pygame.event.get()
+BACKGROUND_COLOR = (255,255,255)
+FONT_NAME = "Georgia"
 
-    for event in events:
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
+TEXT_ANTIALIAS = True
+TEXT_COLOR = (0,100,0)
 
-    business(program_state=program_state)
+HEADER_BOLD = True
+HEADER_FONTSIZE = 18
 
 
 def gui(program_state, business, screen_size):
   pygame.init()
   screen = pygame.display.set_mode(screen_size)
-  main_loop(program_state, business)
+
+  gui = Gui(program_state, business, screen)
+  gui.main_loop()
+  main_loop(program_state, business, screen)
+
+
+
+class Gui:
+  def __init__(self, program_state, business, screen):
+    self.business = business
+    self.header_font = pygame.font.SysFont(FONT_NAME, HEADER_FONTSIZE, HEADER_BOLD)
+    self.header_text = self.header_font.render("Hello", TEXT_ANTIALIAS, TEXT_COLOR)
+    self.messages = {}
+    self.program_state = program_state
+    self.screen = screen
+
+  def main_loop(self):
+    while True:
+      events = pygame.event.get()
+
+      for event in events:
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+
+      self.run_business()
+      self.update()
+
+  def run_business(self):
+    self.messages = self.business(program_state=self.program_state)
+
+  def update(self):
+    self.screen.fill(BACKGROUND_COLOR)
+    self.screen.blit(self.header_text, (200, 200))
+    pygame.display.flip()
+
 
