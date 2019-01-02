6cdae8e8bbbc7d2477ef68e52b6501773cfcda36
M E <matthias.engh@gmail.com>
2019-01-02 14:30:13 +0100

    start adding elements to client gui

--- client.py
+++ client.py
@@ -4,16 +4,20 @@ import sys
 import time
 
 
-def business_procedure(**kwargs):
-  program_state = kwargs['program_state']
-  client_socket = program_state['client_socket']
-  client_socket.connect(program_state['connection_details'])
+def connect(client_socket, connection_details):
+  client_socket.connect(connection_details)
   print client_socket.recv(1024)
   client_socket.close()
   for i in range(3):
     print "shutting down in ", 3 - i, "seconds.."
     time.sleep(1)
   sys.exit()
+
+
+def business_procedure(**kwargs):
+  program_state = kwargs['program_state']
+  if False:
+    connect(program_state['client_socket'], program_state['connection_details'])
 
 def business_function():
   return lambda **kwargs: business_procedure(**kwargs)
@@ -29,5 +33,21 @@ def program_state():
 
 
 screen_size = (600, 500)
-painter = gui.Painter(screen_size)
+
+class ClientPainter(gui.Painter):
+  def update(self, messages):
+    pass
+
+painter = ClientPainter(screen_size)
+
+client_font = 'Georgia'
+client_title = gui.TextField(
+  bold=True,
+  font=client_font,
+  pos=(10,10),
+  font_size=20,
+  text="Client"
+)
+painter.add_element("title", client_title)
+
 gui.gui(program_state(), business_function(), painter)
